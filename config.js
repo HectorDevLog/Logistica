77963152b974ff41d3b3de4401678d7bb0b600ce
hectortc <hectortorres262011@gmail.com>
2026-05-21 13:47:01 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,8 @@
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwAQMdYWwpPYHcZKVXygyok7TTcsDzKHnmsfdW5mPNSUZ0X6ecBujd8O5N8U82P2PS0Xg/exec";
+
+// Al cargar cualquier página, si el usuario hace clic en un enlace del menú, limpiamos la memoria
+document.querySelectorAll('.navbar a, .menu-link').forEach(enlace => {
+  enlace.addEventListener('click', () => {
+    sessionStorage.clear();
+  });
+});
